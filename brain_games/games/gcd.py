e08f2eec8b62672728cd509d645bedc846547823
Farit Gabidullin <cranky90@rambler.ru>
2021-04-04 18:07:21 +0300

    feat: шаг 7 проекта, игра gcd

---
+++ brain_games/games/gcd.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import random
+from math import gcd
+from ..game_engine import engine
+
+
+def start_game():
+    def instruction():
+        return 'What is the result of the expression?'
+
+    def gcd_game():
+        random.seed()
+        first_var = random.randint(1, 99)
+        second_var = random.randint(1, 99)
+        gcd_var = gcd(first_var, second_var)
+        question = '{} {}'.format(str(first_var), str(second_var))
+        answer = str(gcd_var)
+        return {'question': question, 'answer': answer}
+    engine(instruction(), gcd_game)
---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from ..games.gcd import start_game
+
+
+def main():
+    start_game()
+
+
+if __name__ == '__main__':
+    main()
