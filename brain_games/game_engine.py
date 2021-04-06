948fe806e9931a082bd612f9dc524eb7e17f9d99
Farit Gabidullin <cranky90@rambler.ru>
2021-04-06 21:12:30 +0300

    fix: Упростить функцию engine

--- brain_games/game_engine.py
+++ brain_games/game_engine.py
@@ -8,11 +8,6 @@ def welcome_user():
 
 
 def engine(instruction, game):
-    def question(inner_game):
-        return inner_game['question']
-
-    def correct_answer(inner_game):
-        return inner_game['answer']
     player_name = welcome_user()
     print("Hello, {}".format(player_name))
     print(instruction)
@@ -21,14 +16,14 @@ def engine(instruction, game):
     while need_next_question and step < 3:
         step += 1
         one_game_object = game()
-        print('Question: ' + question(one_game_object))
+        print('Question: ' + one_game_object['question'])
         answer = prompt.string("Your answer: ")
-        if answer.lower() == correct_answer(one_game_object):
+        if answer.lower() == one_game_object['answer']:
             print('Correct')
         else:
             template_message = "'{}' is wrong answer ;(. Correct answer was {}."
             print(template_message.format(
-                answer, correct_answer(one_game_object)))
+                answer, one_game_object['answer']))
             print("Let's try again, {}!".format(player_name))
             need_next_question = False
     if need_next_question:
